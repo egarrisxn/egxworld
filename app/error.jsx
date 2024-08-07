533dd34937a880b8773f7cc0f7a5816c2db52456
EGARRISXN <egarrisxn@gmail.com>
2024-08-06 21:31:25 -0400

    fixed a bug

--- app/error.jsx
+++ app/error.jsx
@@ -1,15 +1,17 @@
 'use client'
+import {Button} from '@/components/ui/button'
 
 export default function Error({error, reset}) {
+  console.log({error})
   return (
-    <div className='mx-auto flex min-h-screen flex-col items-center justify-center text-center'>
+    <div className='mx-auto flex flex-col items-center justify-center text-center'>
       <p className='my-4 text-2xl font-semibold'>Something went wrong!</p>
-      <button onClick={reset} className='rounded-lg border-2 bg-blue-500 px-8 shadow-lg'>
+      <Button
+        onClick={reset}
+        className='rounded-lg border-2 bg-blue-500 px-8 py-2 text-white shadow-lg'
+      >
         Refresh
-      </button>
-      <pre className='line-clamp-1 flex w-full max-w-40 flex-row flex-wrap text-base'>
-        {error.message}
-      </pre>
+      </Button>
     </div>
   )
 }
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,17 +1,13 @@
 import Link from 'next/link'
-import {buttonVariants} from '@/components/ui/button'
-// import {Button} from '@/components/ui/button'
+import {Button} from '@/components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>
+    <div className='flex flex-col items-center justify-center'>
       <p className='my-4 text-2xl font-semibold'>Page not found!</p>
-      <Link href='/' className={buttonVariants({variant: 'outline'})}>
-        Home
-      </Link>
-      {/* <Button asChild className='rounded-lg border-2 bg-blue-500 px-8 shadow-lg'>
+      <Button asChild className='rounded-lg border-2 bg-blue-500 px-8 shadow-lg'>
         <Link href='/'>Home</Link>
-      </Button> */}
+      </Button>
     </div>
   )
 }
